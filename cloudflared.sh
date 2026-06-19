52755b4f8e1cc4707eddcc6092ebc7ae3eb8d443
gregorioaugustus-commits <gregorioaugustus@gmail.com>
2026-06-19 02:01:31 -0300

    Update cloudflared.sh

--- cloudflared.sh
+++ cloudflared.sh
@@ -1,6 +1,13 @@
 apt-get update -y && apt-get upgrade -y
-mkdir -p --mode=0755 /usr/share/keyrings
-curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | tee /etc/apt/sources.list.d/cloudflared.list
-apt-get update -y && apt-get install cloudflared -y
-cloudflared service install eyJhIjoiYmQ4MWUwMWMzODg1Y2MwMjFlOTM1N2MwMmRlYjZlNGEiLCJ0IjoiMWI0ZmZiNTAtNjQ1Ni00MzBmLWFjODAtYzhmYzIxOTA0MzI2IiwicyI6Ik5EZGxNVFEyTkRRdE9URmlaUzAwTURrNExUaGhPR1F0WmpSaVpqUmxOVFUyTXpsbSJ9
+
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
+
+sudo cloudflared service install eyJhIjoiYmQ4MWUwMWMzODg1Y2MwMjFlOTM1N2MwMmRlYjZlNGEiLCJ0IjoiMWI0ZmZiNTAtNjQ1Ni00MzBmLWFjODAtYzhmYzIxOTA0MzI2IiwicyI6Ik5EZGxNVFEyTkRRdE9URmlaUzAwTURrNExUaGhPR1F0WmpSaVpqUmxOVFUyTXpsbSJ9
